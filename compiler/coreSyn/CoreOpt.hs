b726f715cfef0e52476b8866308c857c60331ae0
Joachim Breitner <mail@joachim-breitner.de>
2017-08-04 15:34:11 -0400

    Prevent inlining of loopified programs
    Previously, a recursive function is not inlineable. After loopification,
    it turns into a non-recursive function, and suddenly it is. While this
    is in general desirable, it has many knock-on effects, which makes it
    hard to evaluate and debug loopification. Therefore, this commit (tries to)
    prevent this inlining. When this results in no unfixable regressions,
    then we can tackle the next step.

--- compiler/coreSyn/CoreOpt.hs
+++ compiler/coreSyn/CoreOpt.hs
@@ -672,7 +672,11 @@ loopificationJoinPointBinding_maybe bndr rhs
                      localiseId $
                      bndr
         -- RULES etc stay with bindr'
-        bndr' = zapIdTailCallInfo bndr
+        -- Also, previously, the function was recursive, and hence not inlineable.
+        -- To tread with caution, let's keep it this way
+        bndr' = (`setIdUnfolding` noUnfolding) $
+                zapIdTailCallInfo $
+                bndr
     in  Just (bndr', join_bndr, mkLams bndrs body)
 
   | otherwise
